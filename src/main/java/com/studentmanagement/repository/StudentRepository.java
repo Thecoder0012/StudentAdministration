6c66badd9cb2b67ced3ab8e38a0746e2cf95f272
Mardin <mardineliassi57@gmail.com>
2022-02-26 22:13:28 +0100

    Small changes in html and css.

--- src/main/java/com/studentmanagement/repository/StudentRepository.java
+++ src/main/java/com/studentmanagement/repository/StudentRepository.java
@@ -1,7 +1,8 @@
 package com.studentmanagement.repository;
-
 import com.studentmanagement.models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface StudentRepository extends JpaRepository<Student,Long> { // tager imod 2 parametre, entity altså vores table og typen primary key.
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
 }
