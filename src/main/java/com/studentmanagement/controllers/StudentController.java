b3c80353c6caa717566a95d71fb7443d16d6a509
Mardin <mardineliassi57@gmail.com>
2022-03-24 00:11:21 +0100

    Deleted a method in service. Removed autowired (field injection)

--- src/main/java/com/studentmanagement/controllers/StudentController.java
+++ src/main/java/com/studentmanagement/controllers/StudentController.java
@@ -2,7 +2,6 @@ package com.studentmanagement.controllers;
 
 import com.studentmanagement.models.Student;
 import com.studentmanagement.services.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,37 +12,40 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController {
 
-    @Autowired
-    private StudentService studentService;
+    private final StudentService studentService;
 
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
 
     @GetMapping("/students")
-    public String studentList(Model model){
+    public String studentList(Model model) {
         model.addAttribute("students", studentService.getStudents());
         return "students";
     }
-  
+
     @GetMapping("/students/new")
-    public String addStudent(Model model){
+    public String addStudent(Model model) {
         Student student = new Student();
-        model.addAttribute("addStudents",student);
+        model.addAttribute("addStudents", student);
         return "addStudent";
     }
 
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("addStudents")Student student){
+    public String saveStudent(@ModelAttribute("addStudents") Student student) {
         studentService.saveStudent(student);
         return "redirect:/students";
     }
+
     @GetMapping("/students/update/{id}")
-    public String updateStudent(@PathVariable Long id, Model model){
+    public String updateStudent(@PathVariable Long id, Model model) {
         model.addAttribute("student", studentService.getStudent(id));
         return "updateStudent";
     }
 
     @PostMapping("/students/{id}")
-    public String editStudent(@PathVariable Long id, @ModelAttribute("student")Student student){
+    public String editStudent(@PathVariable Long id, @ModelAttribute("student") Student student) {
 
         Student students = studentService.getStudent(id); // getting the student from the database
         students.setId(id);
@@ -51,13 +53,13 @@ public class StudentController {
         students.setLastName(student.getLastName());
         students.setAge(student.getAge());
         students.setEmail(student.getEmail());
-        studentService.updateStudent(students);
+        studentService.saveStudent(students);
 
         return "redirect:/students";
     }
 
     @GetMapping("/students/{id}")
-    public String deleteStudent(@PathVariable Long id){
+    public String deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
         return "redirect:/students";
     }
--- src/main/java/com/studentmanagement/services/StudentService.java
+++ src/main/java/com/studentmanagement/services/StudentService.java
@@ -2,34 +2,30 @@ package com.studentmanagement.services;
 
 import com.studentmanagement.models.Student;
 import com.studentmanagement.repository.StudentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-@org.springframework.stereotype.Service
+
+@Service
 public class StudentService {
 
-    @Autowired
-    private StudentRepository studentRepository;
+    private final StudentRepository studentRepository;
 
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
-
     }
 
-    public Student saveStudent(Student student) {
-        return studentRepository.save(student);
+    public void saveStudent(Student student) {
+        studentRepository.save(student);
     }
 
     public Student getStudent(Long id) {
         return studentRepository.findById(id).get();
     }
-
-
-    public Student updateStudent(Student student) {
-        return studentRepository.save(student);
-    }
-
 
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
